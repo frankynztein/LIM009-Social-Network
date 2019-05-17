3e6b6bb13c3a54e38bf5a0453dde086c34ada1fb
frankynztein <arellanfranciree@gmail.com>
2019-05-16 20:38:07 -0500

    CSS ViewLogin

--- src/view/viewLogIn.js
+++ src/view/viewLogIn.js
@@ -8,26 +8,34 @@ const changeHash = (hash) =>  {
 export const viewLogin = () => {
     const root= document.getElementById('content')
     const loginPage = `
-    <main class="fondo-login" >  
+    <main>  
       <div id="login-container">
-        <figure>
-           
-        </figure>
-        <div id='login'>
-          <img class="center"<img src= 'https://i.postimg.cc/rpPnvrL1/fbook.png'>
-          <h3 class="center">¡Bienvenido, comensal!</h3>
-            <form id="login-user">
-                <input class="d-block input-w" type="email" id="email-login" placeholder="Email">
-                <input class="d-block input-w" type="password" id="password-login" placeholder="Password">
-                <button class="d-block btn-login btn-width" id="login-btn">Log in</button>
-                <p class="m-auto">O bien ingresa con...</p>
-                <a id="googleBtn"><img src="../images/search.svg" alt="Google" style="width:30px;"></img></a>
-                <a id="fbBtn"><img src="../images/facebook-logo-in-circular-button-outlined-social-symbol.svg" alt="Facebook" style="width:30px;"></img></a>
-            </form>
-          <p class="m-auto">¿No tienes una cuenta? <a id="myBtn" class="register" href="#/register">Regístrate.</a></p>
+        <div class="row-login">
+          <div class="column-login">
+            <div class="column-login-image">
+                <figure>
+                    <img class="s-size b-size" src="../images/undraw_Bibliophile_hwqc.svg" alt="">
+                </figure>
+            </div>
+          </div>
+          <div class="column-login">
+            <div id='login' class="column-login-login">
+                <img class="center" src= 'https://i.postimg.cc/rpPnvrL1/fbook.png'>
+                <h1 class="center">¡Bienvenido, comensal!</h1>
+                    <form id="login-user">
+                        <input class="d-block input-w" type="email" id="email-login" placeholder="Email">
+                        <input class="d-block input-w" type="password" id="password-login" placeholder="Password">
+                        <button class="d-block btn-login btn-width" id="login-btn">Log in</button>
+                        <p class="m-auto">O bien ingresa con...</p>
+                        <a id="googleBtn"><img class="social-login" src="../images/search.svg" alt="Google"></img></a>
+                        <a id="fbBtn"><img class="social-login" src="../images/facebook-logo-in-circular-button-outlined-social-symbol.svg" alt="Facebook"></img></a>
+                    </form>
+                <p class="m-auto">¿No tienes una cuenta? <a id="myBtn" class="register" href="#">Regístrate.</a></p>
+            </div>
+          </div>
         </div>
       </div>
-      </main>`;
+    </main>`;
     root.innerHTML = loginPage;
   
     const btnLogInEmail = root.querySelector('#login-btn');
