d23ced8c62110ad36d28cf390a13a76069b37f8b
NardaC <nardac6@gmail.com>
2019-05-24 18:28:37 -0500

    editar, borrar post solo del usuario activo

--- src/view/login.js
+++ src/view/login.js
@@ -4,6 +4,12 @@ import {facebookLogin,googleLogin,createUser,userSesionActive,signInUser, exit} 
 const changeHash = (hash) =>  {
   location.hash = hash;
 }
+export const showErrorMessage = (errorText) => {
+  let errorMessage = document.createElement('p');
+  let textError = document.createTextNode(errorText);
+  errorMessage.appendChild(textError);
+  document.getElementById('login-error-alert').appendChild(errorMessage);
+};
 
 export const viewLogin = () => {
     const root= document.getElementById('content')
@@ -14,7 +20,7 @@ export const viewLogin = () => {
           <div class="column-login">
             <div class="column-login-image">
                 <figure>
-                    <img class="s-size b-size" src="assets/undraw_Bibliophile_hwqc.svg" alt="">
+                    <img class="s-size b-size" src="assets/undraw_street_food_hm5i.svg" alt="">
                 </figure>
             </div>
         </div>
@@ -25,7 +31,8 @@ export const viewLogin = () => {
             <form id="login-user">
                 <input class="d-block input-w" type="email" id="email-login" placeholder="Email">
                 <input class="d-block input-w" type="password" id="password-login" placeholder="Password">
-                <button class="d-block btn-login btn-width" id="login-btn">Log in</button>
+                <section id= "login-error-alert" class= "error-alert"></section>
+                <button class="d-block btn-login btn-width" id="login-btn">Inicia sesión</button>
                 <p class="m-auto">O bien ingresa con...</p>
                 <a id="fbBtn"><img class="social-btn" src="assets/facebook-logo-in-circular-button-outlined-social-symbol.svg" alt="Facebook"></img></a>
                 <a id="googleBtn"><img class="social-btn" src="assets/search.svg" alt="Google"></img></a>
@@ -41,11 +48,27 @@ export const viewLogin = () => {
     const btnLogInEmail = root.querySelector('#login-btn');
     const emailLogInEmail = root.querySelector('#email-login');
     const passwordLogInEmail = root.querySelector('#password-login');
+
+    
     btnLogInEmail.addEventListener('click', (event) => {
       event.preventDefault();
       signInUser(emailLogInEmail.value, passwordLogInEmail.value)
       .then(() => changeHash('#/profile'))
-      .catch((error) => console.log(error));
+      .catch((error) => {
+        let errorCode = error.code;
+        console.log(error.code);
+        if (errorCode === 'auth/user-not-found') {
+          showErrorMessage('Usuario no registrado.');
+        } else if (errorCode === 'auth/weak-password') {
+          showErrorMessage('La clave debe tener al menos 6 dígitos.');
+        } else if (errorCode === 'auth/wrong-password') {
+          showErrorMessage('Clave inválida');
+        } else if (errorCode === 'auth/invalid-email') {
+          showErrorMessage('Correo electrónico inválido.');
+        } else {
+          showErrorMessage(errorCode);
+        }        
+      });
     });
       
     const loginFacebook = root.querySelector('#fbBtn');
@@ -66,3 +89,4 @@ export const viewLogin = () => {
     });
     return root;
 }
+
